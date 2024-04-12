227f135ef63d049f2283d6dbe1fab4d2f0fe5d20
nael.lilik <nael.lilik@gmail.com>
2024-04-12 12:18:18 +0700

    progres

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import puppeteer from 'puppeteer';
   await page.type('.devsite-search-field', 'automate beyond recorder');
 
   // Wait and click on first result
-  const searchResultSelector = '.devsite-result-item-link';
+  const searchResultSelector = '.devsite-result-item-link'; // document.querySelector("#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.TIHn2 > div > div.lMbq3e > h2")
   await page.waitForSelector(searchResultSelector);
   await page.click(searchResultSelector);
 
